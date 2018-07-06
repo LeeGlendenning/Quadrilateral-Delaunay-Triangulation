5cf2bacde12632e3c12b8576f7f6c6bae9866e45
Lee Glendenning <glen9190@mylaurier.ca>
2018-07-05 21:59:13 -0400

    Add test points and fix quad vertex ordering to be clockwise

--- src/dt/Test/Test.java
+++ src/dt/Test/Test.java
@@ -18,13 +18,30 @@ public class Test {
     public static void main(String[] args) {
         
         ArrayList<Point> pts = new ArrayList();
+        //b3p case example: (100,100), (400,300), (200,500), diamond, center = (23, 15)
         pts.add(new Point(100, 100));
-        //pts.add(new Point(500, 300));
+        pts.add(new Point(200, 200));
+        pts.add(new Point(150, 350));
+        pts.add(new Point(400, 300));
         pts.add(new Point(200, 300));
+        pts.add(new Point(200, 500));
         
-        //Point[] quad = {new Point(0,0), new Point(40,10), new Point(30,40), new Point(10,31)};
-        Point[] quad = {new Point(0,0), new Point(40,10), new Point(30,40), new Point(10,30)};
-        Quadrilateral q = new Quadrilateral(quad);
+        
+        // Quad1
+        //Point[] quad = {new Point(0,0), new Point(10,30), new Point(30,40), new Point(40,10)};
+        Point center = new Point(13, 17);
+        
+        // Quad2
+        //Point[] quad = {new Point(20,0), new Point(10,30), new Point(30,40), new Point(40,10)};
+        
+        // Square
+        Point[] quad = {new Point(0,0), new Point(0,20), new Point(20,20), new Point(20,0)};
+        
+        // Diamond
+        //Point[] quad = {new Point(0,20), new Point(20,40), new Point(40,20), new Point(20,0)};
+        
+        //Quadrilateral q = new Quadrilateral(quad);
+        Quadrilateral q = new Quadrilateral(quad, center);
         
         DelaunayTriangulation dt = new DelaunayTriangulation(q, pts);
         //dt.drawDT();
